f92f91a64dbf5fae5382ac0745e24924249cf8ea
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-07-28 12:36:03 -0700

    fix: types

--- src/types.ts
+++ src/types.ts
@@ -38,10 +38,15 @@ type RenderableTemplate<T> = {
     [RENDERABLE]: typeof RENDERABLE_TEMPLATE;
     literals: TemplateStringsArray;
     template: Template | null;
-    values: (RenderableValue<T> | RenderableValue<T>[])[];
+    values: RenderableValue<T>[];
 };
 
-type RenderableValue<T = unknown> = Attributes | Readonly<Attributes> | Readonly<Attributes[]> | Effect<T> | Primitive | Renderable;
+type RenderableValue<T = unknown> =
+    Attributes | Attributes[] |
+    Readonly<Attributes> | Readonly<Attributes[]> |
+    Effect<T> | Effect<T>[] |
+    Primitive | Primitive[] |
+    Renderable | Renderable[];
 
 type Template = {
     fragment: DocumentFragment;
