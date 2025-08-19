7ab38f9e758e9ae6c6a531e60fecb91726950823
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-08-19 01:08:53 -0700

    fix: svg attributes

--- src/svg.ts
+++ src/svg.ts
@@ -2,15 +2,15 @@ import html from './html';
 
 
 const svg = html.bind(null) as typeof html & {
-    sprite: (symbol: string) => ReturnType<typeof html>
+    sprite: (href: string) => ReturnType<typeof html>
 };
 
-svg.sprite = (symbol: string) => {
-    if (symbol[0] !== '#') {
-        symbol = '#' + symbol;
+svg.sprite = (href: string) => {
+    if (href[0] !== '#') {
+        href = '#' + href;
     }
 
-    return html`<svg><use xlink:href='${symbol}' /></svg>`;
+    return html`<svg><use ${{ ['href']: href }} /></svg>`;
 };
 
 
