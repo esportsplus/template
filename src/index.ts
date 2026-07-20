1ebf45c8d1e15a776f8c4ea7a7591c7c3a87afd1
Israel Casillas Jr <ICJR@users.noreply.github.com>
2026-07-20 07:47:16 -0700

    refactor(index): drop node prototype pre-allocation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import { CLEANUP, STORE } from './constants';
-
-
-// Pre-allocate on Node prototype to optimize property access
-if (typeof Node !== 'undefined') {
-    (Node.prototype as any)[CLEANUP] = null;
-    (Node.prototype as any)[STORE] = null;
-}
-
-
 export * from './attributes';
 export * from './event';
 export * from './hmr';
